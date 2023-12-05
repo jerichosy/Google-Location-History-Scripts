b900ae3c87f0c61a6f6a82c641a9ba53d6b5deb6
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-12-06 05:32:26 +0800

    Make raw timestamp human readable and convert to user-specified timezone

--- locations_within_radius.py
+++ locations_within_radius.py
@@ -1,5 +1,14 @@
+import argparse
 import json
 import math
+from datetime import datetime
+
+import pytz
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Process locations within a certain radius and adjust timestamps for a timezone.")
+parser.add_argument('-tz', '--timezone', help='Timezone to convert the timestamp to, e.g., "Asia/Manila".', default="")
+args = parser.parse_args()
 
 # Function to calculate distance between two points using Haversine formula
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -22,6 +31,24 @@ def haversine_distance(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     return R * c
+
+# Function to convert timestamp to a different timezone and make it human-readable
+def convert_timestamp(time_string, timezone_name):
+    # Try parsing the timestamp with and without milliseconds
+    for fmt in ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ"]:
+        try:
+            utc_time = datetime.strptime(time_string, fmt)
+        except ValueError:
+            continue
+        else:
+            break
+    else:
+        raise ValueError(f"time data '{time_string}' does not match format with or without milliseconds")
+
+    utc_time = utc_time.replace(tzinfo=pytz.utc)
+    target_timezone = pytz.timezone(timezone_name)
+    local_time = utc_time.astimezone(target_timezone)
+    return local_time.strftime("%Y-%m-%d %H:%M:%S %Z (%A)")
 
 # Load JSON data (assuming it's stored in a file named 'Records.json' in the current directory)
 FILENAME = 'Records.json'
@@ -49,5 +76,14 @@ for location in data['locations']:
 
     # If the location is within the specified radius, print it
     if distance <= radius_km:
-        # print(f"Distance from user: {distance} km")
-        print(f"Location within {radius_km} km: lat={location_lat}, long={location_long}, timestamp={location['timestamp']}")
+        if args.timezone:
+            # Convert the timestamp
+            try:
+                human_readable_timestamp = convert_timestamp(location['timestamp'], args.timezone)
+            except Exception as e:
+                print(f"Error converting timestamp: {e}")
+                human_readable_timestamp = 'N/A'
+        else:
+            human_readable_timestamp = location['timestamp']
+
+        print(f"Location within {radius_km} km: lat={location_lat}, long={location_long}, timestamp={human_readable_timestamp}")
