674bbd2266c08a7b4543ba1236c568fe2bc58309
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-12-06 06:15:15 +0800

    Add option to print first/last instance per day

--- locations_within_radius.py
+++ locations_within_radius.py
@@ -1,14 +1,20 @@
 import argparse
 import json
 import math
+from collections import defaultdict
 from datetime import datetime
 
 import pytz
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Process locations within a certain radius and adjust timestamps for a timezone.")
-parser.add_argument('-tz', '--timezone', help='Timezone to convert the timestamp to, e.g., "Asia/Manila".', default="")
+parser.add_argument('-tz', '--timezone', help='Timezone to convert the timestamp to, e.g., Asia/Manila')
+parser.add_argument('--perday', action='store_true', help='Print the first and last location instances for each day')
 args = parser.parse_args()
+
+# Check if perday is True but timezone is not set
+if args.perday and not args.timezone:
+    parser.error("--perday requires --timezone to be set.")
 
 # Function to calculate distance between two points using Haversine formula
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -61,6 +67,9 @@ user_lat = float(input("Please enter latitude: "))
 user_long = float(input("Please enter longitude: "))
 radius_km = float(input("Please enter radius in kilometers: "))
 
+# Create a dictionary to store lists of locations for each date
+daily_locations = defaultdict(list)
+
 for location in data['locations']:
     # Convert E7 format to standard latitude and longitude
     try:
@@ -74,16 +83,37 @@ for location in data['locations']:
     # Calculate distance from user's location
     distance = haversine_distance(user_lat, user_long, location_lat, location_long)
 
-    # If the location is within the specified radius, print it
+    # If the location is within the specified radius, process it
     if distance <= radius_km:
         if args.timezone:
             # Convert the timestamp
             try:
                 human_readable_timestamp = convert_timestamp(location['timestamp'], args.timezone)
+                # Extract the date (local time) from the human_readable_timestamp
+                local_date = human_readable_timestamp.split()[0]
+                # Add location to its corresponding date's list
+                daily_locations[local_date].append((distance, human_readable_timestamp, location_lat, location_long))
             except Exception as e:
-                print(f"Error converting timestamp: {e}")
+                print(f"Error converting and processing timestamp: {e}")
+                # Set to 'N/A' because we don't want to skip any location instance (the lat/long will be useful for debugging)
+                # and we want to be explicit, so we don't accidentally use the wrong timestamp
                 human_readable_timestamp = 'N/A'
         else:
             human_readable_timestamp = location['timestamp']
 
-        print(f"Location within {radius_km} km: lat={location_lat}, long={location_long}, timestamp={human_readable_timestamp}")
+        # If not in first/last mode, print every location
+        if not args.perday:
+            print(f"Location within {radius_km} km: lat={location_lat}, long={location_long}, timestamp={human_readable_timestamp}")
+
+# If in first/last mode, process the dictionary to print the first and last location for each day
+if args.perday:
+    for date, locations in daily_locations.items():
+        if len(locations) == 1:
+            # If there's only one location for the day, print it
+            _, timestamp, lat, long = locations[0]
+            print(f"Location within {radius_km} km: lat={lat}, long={long}, timestamp={timestamp}")
+        else:
+            # Print the first and last location for the day
+            for _, timestamp, lat, long in (locations[0], locations[-1]):
+                print(f"Location within {radius_km} km: lat={lat}, long={long}, timestamp={timestamp}")
+        print()
